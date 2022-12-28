97fbacd087a85211bf2ccf12d00e9d4a4241aa7f
Amrit <amritkumarjha7777@gmail.com>
2022-12-28 21:19:37 +0530

    changes for hosting on gh

--- src/App.js
+++ src/App.js
@@ -14,18 +14,18 @@ export default function App() {
   return (
     <div>
       {/* <h1>Jai Shree Ram</h1> */}
-      <Router>
+      <Router basename="/fastnews">
         <LoadingBar color='#f11946' progress={progress} height={3} />
         <Navbar />
         <Switch>
-          <Route exact path="/"><News setProgress={setProgress} key={1} pageSize={15} country="in" apiKey={apiKey} category="general" /></Route>
-          <Route exact path="/business"><News setProgress={setProgress} key={2} pageSize={15} country="in" apiKey={apiKey} category="business" /></Route>
-          <Route exact path="/entertainment"><News setProgress={setProgress} key={3} pageSize={15} country="in" apiKey={apiKey} category="entertainment" /></Route>
-          <Route exact path="/general"><News setProgress={setProgress} key={4} pageSize={15} country="in" apiKey={apiKey} category="general" /></Route>
-          <Route exact path="/health"><News setProgress={setProgress} key={5} pageSize={15} country="in" apiKey={apiKey} category="health" /></Route>
-          <Route exact path="/science"><News setProgress={setProgress} key={6} pageSize={15} country="in" apiKey={apiKey} category="science" /></Route>
-          <Route exact path="/sports"><News setProgress={setProgress} key={7} pageSize={15} country="in" apiKey={apiKey} category="sports" /></Route>
-          <Route exact path="/technology"><News setProgress={setProgress} key={8} pageSize={15} country="in" apiKey={apiKey} category="technology" /></Route>
+          <Route exact path="/fastnews"><News setProgress={setProgress} key={1} pageSize={15} country="in" apiKey={apiKey} category="general" /></Route>
+          <Route path="/business"><News setProgress={setProgress} key={2} pageSize={15} country="in" apiKey={apiKey} category="business" /></Route>
+          <Route path="/entertainment"><News setProgress={setProgress} key={3} pageSize={15} country="in" apiKey={apiKey} category="entertainment" /></Route>
+          <Route path="/general"><News setProgress={setProgress} key={4} pageSize={15} country="in" apiKey={apiKey} category="general" /></Route>
+          <Route path="/health"><News setProgress={setProgress} key={5} pageSize={15} country="in" apiKey={apiKey} category="health" /></Route>
+          <Route path="/science"><News setProgress={setProgress} key={6} pageSize={15} country="in" apiKey={apiKey} category="science" /></Route>
+          <Route path="/sports"><News setProgress={setProgress} key={7} pageSize={15} country="in" apiKey={apiKey} category="sports" /></Route>
+          <Route path="/technology"><News setProgress={setProgress} key={8} pageSize={15} country="in" apiKey={apiKey} category="technology" /></Route>
         </Switch>
       </Router>
     </div>
